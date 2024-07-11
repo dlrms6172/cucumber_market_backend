864bdbed878facb4d1a05eadd1d4dcb29311a5b8
leekun <dlrms6172@naver.com>
2024-07-11 19:00:56 +0900

    new.초기설정

---
+++ src/main/java/com/cucumber/market/api/CucumberMarketApiApplication.java
@@ -0,0 +1,11 @@
+package com.cucumber.market.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberMarketApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CucumberMarketApiApplication.class, args);
+    }
+}
