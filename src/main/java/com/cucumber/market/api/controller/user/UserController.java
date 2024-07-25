d05164918f0889b312df4efcb396b43620720661
이근 <dlrms617292@gmail.com>
2024-07-25 17:34:03 +0900

    스키마 변경,gradle 추가,구글 로그인 서비스 구현(2)

--- src/main/java/com/cucumber/market/api/controller/user/UserController.java
+++ src/main/java/com/cucumber/market/api/controller/user/UserController.java
@@ -1,16 +1,34 @@
 package com.cucumber.market.api.controller.user;
 
+import com.cucumber.market.api.dto.user.UserDto;
+import com.cucumber.market.api.service.user.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private HttpHeaders headers;
+    private Map<String, Object> body = new LinkedHashMap<String, Object>() {
+        {
+            put("resultCode", 200);
+            put("resultMsg", "success");
+        }
+    };
+
+    @Autowired
+    UserService userService;
+
     @Value("${google.client.auth-url}")
     private String googleAuthUrl;
 
@@ -26,30 +44,49 @@ public class UserController {
     @Value("${google.client.scope}")
     private String googleScope;
 
+    @GetMapping(value="/signin")
+    public ResponseEntity signInController(@Valid UserDto.signInDto dto){
 
-    @GetMapping(value="/google")
-    public String loginUrlGoogle(){
-        Map<String,Object> params = new HashMap<>();
-        params.put("scope",googleScope);
-        params.put("response_type","code");
-        params.put("client_id",googleClientId);
-        params.put("redirect_uri",googleCallBackUrl);
+        body.put("data",userService.signInService(dto));
 
-        String parameterString=params.entrySet().stream()
-                .map(x->x.getKey()+"="+x.getValue())
-                .collect(Collectors.joining("&"));
-        String redirectURL=googleAuthUrl+"?"+parameterString;
-        System.out.println("redirectURL = " + redirectURL);
-
-        return redirectURL;
+        return new ResponseEntity(body, headers, HttpStatus.OK);
     }
 
-    @GetMapping("/singin/google/callback")
-    public String test(String code){
+    @GetMapping("/singin/callback/{platform}")
+    public ResponseEntity signInCallBackController(@Valid UserDto.signInCallBackDto dto){
 
-        System.out.println(code);
 
-        return code;
+        body.put("data",userService.signInCallBackService(dto));
+
+        String GOOGLE_TOKEN_REQUEST_URL="https://oauth2.googleapis.com/token";
+        RestTemplate restTemplate=new RestTemplate();
+        Map<String, Object> params = new HashMap<>();
+        params.put("code", dto.getCode());
+        params.put("client_id", googleClientId);
+        params.put("client_secret", googleClientPw);
+        params.put("redirect_uri", googleCallBackUrl);
+        params.put("grant_type", "authorization_code");
+
+        ResponseEntity<Map> responseEntity=restTemplate.postForEntity(GOOGLE_TOKEN_REQUEST_URL,
+                params,Map.class);
+
+
+        if(responseEntity.getStatusCode() == HttpStatus.OK){
+            String GOOGLE_USERINFO_REQUEST_URL="https://www.googleapis.com/oauth2/v1/userinfo";
+            String accessToken = responseEntity.getBody().get("access_token").toString();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization","Bearer "+accessToken);
+
+            //HttpEntity를 하나 생성해 헤더를 담아서 restTemplate으로 구글과 통신하게 된다.
+            HttpEntity<Map<String, String>> request = new HttpEntity(headers);
+            ResponseEntity<String> response=restTemplate.exchange(GOOGLE_USERINFO_REQUEST_URL, HttpMethod.GET,request,String.class);
+            System.out.println("response.getBody() = " + response.getBody());
+        }
+
+
+        return new ResponseEntity(body, headers, HttpStatus.OK);
+
     }
 
 }
--- src/main/java/com/cucumber/market/api/service/user/UserService.java
+++ src/main/java/com/cucumber/market/api/service/user/UserService.java
@@ -1,4 +1,91 @@
 package com.cucumber.market.api.service.user;
 
+import com.cucumber.market.api.dto.user.UserDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
 public class UserService {
+    private LinkedHashMap<String,Object> result = new LinkedHashMap<>();
+
+    @Value("${google.client.auth-url}")
+    private String googleAuthUrl;
+
+    @Value("${google.client.id}")
+    private String googleClientId;
+
+    @Value("${google.client.pw}")
+    private String googleClientPw;
+
+    @Value("${google.client.callback-url}")
+    private String googleCallBackUrl;
+
+    @Value("${google.client.scope}")
+    private String googleScope;
+
+    @Value("${google.client.token-request-url}")
+    private String googleTokenRequestUrl;
+
+    /**
+     * 로그인 서비스
+     * @return
+     */
+    public Map signInService(UserDto.signInDto dto) {
+
+        // 구글 로그인 서비스
+        if(dto.getPlatform().equals("google")){
+            Map<String,Object> params = new HashMap<>();
+            params.put("scope",googleScope);
+            params.put("response_type","code");
+            params.put("client_id",googleClientId);
+            params.put("redirect_uri",googleCallBackUrl);
+
+            String parameterString=params.entrySet().stream()
+                    .map(x->x.getKey()+"="+x.getValue())
+                    .collect(Collectors.joining("&"));
+            String redirectURL=googleAuthUrl+"?"+parameterString;
+
+            result.put("signInPage",redirectURL);
+        }
+
+        return result;
+    }
+
+    /**
+     * 로그인 콜백 서비스
+     */
+    public Map signInCallBackService(UserDto.signInCallBackDto dto) {
+
+        // 구글 로그인 서비스
+        if(dto.getPlatform().equals("google")){
+            // 엑세스 토큰 발급
+            RestTemplate restTemplate = new RestTemplate();
+            Map<String, Object> params = new HashMap<>();
+            params.put("code", dto.getCode());
+            params.put("client_id", googleClientId);
+            params.put("client_secret", googleClientPw);
+            params.put("redirect_uri", googleCallBackUrl);
+            params.put("grant_type", "authorization_code");
+
+            ResponseEntity<Map> responseEntity=restTemplate.postForEntity(googleTokenRequestUrl,
+                    params,Map.class);
+
+            // 사용자 정보
+            if(responseEntity.getStatusCode() == HttpStatus.OK){
+
+            }
+            
+        }
+
+        return result;
+    }
+
 }
