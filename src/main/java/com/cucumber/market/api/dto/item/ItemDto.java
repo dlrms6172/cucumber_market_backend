22da64d3921ea0ffc4dd6d77c7f29e296c1e84d1
Yunwoo Jeong <jeongmallro99@gmail.com>
2024-08-25 23:20:47 +0900

    refactor : 상품 수정(#12)
    - 누락된 ItemRepository 추가
    - ItemStatus Enum 추가

---
+++ src/main/java/com/cucumber/market/api/dto/item/ItemDto.java
@@ -0,0 +1,36 @@
+package com.cucumber.market.api.dto.item;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+public class ItemDto {
+
+    @Getter
+    @Setter
+    public static class addItemDto {
+        private Long itemId;
+        @NotBlank
+        private String itemName;
+        @NotBlank
+        private String itemInfo;
+        private LocalDateTime postDate;
+        @NotBlank
+        private int price;
+    }
+
+    @Getter
+    @Setter
+    public static class modifyItemDto {
+        private Long itemId;
+        @NotBlank
+        private String itemName;
+        @NotBlank
+        private String itemInfo;
+        private LocalDateTime updateDate;
+        @NotBlank
+        private int price;
+    }
+}
