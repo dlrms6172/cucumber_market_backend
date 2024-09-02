55ffba2641f8d15f5c38b5c85b4a3377be80e6fe
jeongmallro <jeongmallro99@gmail.com>
2024-09-02 15:07:52 +0900

    Update ItemMapper.java

--- src/main/java/com/cucumber/market/api/mapper/item/ItemMapper.java
+++ src/main/java/com/cucumber/market/api/mapper/item/ItemMapper.java
@@ -24,6 +24,10 @@ public interface ItemMapper {
 
     void updateItemStatus(@Param("itemId") Long itemId, @Param("itemStatus") ItemStatus itemStatus);
 
+    List<Map> selectItems(@Param("itemName") String itemName, @Param("itemStatus") ItemStatus itemStatus);
+
+    void deleteItem(Long itemId);
+    
     void insertLike(@Param("itemId") Long itemId, @Param("memberId") Long memberId);
 
     void deleteLike(@Param("itemId") Long itemId, @Param("memberId") Long memberId);
