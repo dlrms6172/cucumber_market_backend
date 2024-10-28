24cb3664965a76ca74b214e7b551903cf04aa202
jeongmallro <jeongmallro99@gmail.com>
2024-10-28 15:41:14 +0900

    Update CorsConfig.java
    모든 도메인 허용

--- src/main/java/com/cucumber/market/api/common/config/CorsConfig.java
+++ src/main/java/com/cucumber/market/api/common/config/CorsConfig.java
@@ -11,7 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "https://localhost:8080")
+                // .allowedOrigins("http://localhost:8080", "https://localhost:8080")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
